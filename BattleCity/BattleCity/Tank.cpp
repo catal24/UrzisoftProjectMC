5c5dc6839021dcbe9a4b739b9f016624f424ef27
Alecsa Alin <alin.alecsa@student.unitbv.ro>
2024-10-31 13:18:33 +0200

    Class tank getter setter + constructor

--- BattleCity/BattleCity/Tank.cpp
+++ BattleCity/BattleCity/Tank.cpp
@@ -1,1 +1,51 @@
 #include "Tank.h"
+
+
+
+
+Tank::Tank(int xStart, int yStart, int id, bool isBreakable, bool isVisible, int lives, int speed,bool isDead)
+{
+	GameObject(xStart, yStart, id, isBreakable, isVisible);
+	m_lives = lives;
+	m_speed=speed;
+	m_isDead = false;
+}
+
+void Tank::setSpeed(int amount)
+{
+	m_speed = amount;
+}
+
+void Tank::setLives(int amount)
+{
+	m_lives = amount;
+}
+
+void Tank::setIsDead()
+{
+	m_isDead = true;
+}
+
+int Tank::getSpeed() const
+{
+	return m_speed;
+}
+
+int Tank::getLives() const
+{
+	return m_lives;
+}
+
+bool Tank::getIsDead() const
+{
+	return m_isDead;
+}
+
+void Tank::die()
+{
+	setLives(m_lives - 1);
+	if (m_isDead > 0)
+		respawn();
+	else
+		setIsDead();
+}
--- BattleCity/BattleCity/Tank.h
+++ BattleCity/BattleCity/Tank.h
@@ -1,10 +1,29 @@
 #pragma once
-class Tank 
+#include "GameObject.h"
+class Tank :public GameObject
 {
-private:
-	int lives;
-	int speed;
-	bool isDead=false;
+public:
+	Tank(int xStart, int yStart, int id, bool isBreakable, bool isVisible,int lives, int speed, bool isDead=false);
+
+	~Tank();
+	
+	void setSpeed(int amount);
+	void setLives(int amount);
+	void setIsDead();
+	int getSpeed()const;
+	int getLives()const;
+	bool getIsDead()const;
+	void die();
+	void respawn();
+	
+
+
+	private:
+	int m_lives;
+	int m_speed;
+	bool m_isDead;
+	
+
 
 };
 
