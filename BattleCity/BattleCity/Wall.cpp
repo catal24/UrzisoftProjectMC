1c81b2c61cdafbf137a0e3db021cd82e81b11c34
andreirobert04 <barabasrobert3006@gmail.com>
2024-11-07 20:15:06 +0200

    wall inheritance gameobject

--- BattleCity/BattleCity/Wall.cpp
+++ BattleCity/BattleCity/Wall.cpp
@@ -2,13 +2,19 @@
 #include "Wall.h"
 
 
-Wall::Wall(bool isDestructible, bool hasBomb)
-	:m_isDestructible{isDestructible}, m_hasBomb{hasBomb}
+Wall::Wall(int id, int xStart, int yStart, bool isVisible, bool isBreakable, bool hasBomb)
+	:m_id{ id },
+	m_xStart{ xStart },
+	m_yStart{ yStart },
+	m_isBreakable{ isBreakable },
+	m_isVisible{ isVisible },
+	m_hasBomb{ hasBomb }
+
 {}
 
-bool Wall::IsDestructible() const
+bool Wall::IsBreakable() const
 {
-	return m_isDestructible;
+	return m_isBreakable;
 }
 
 
@@ -20,7 +26,7 @@ bool Wall::HasBomb() const
 
 void Wall::SetDestructible(bool destructible)
 {
-	m_isDestructible = destructible;
+	m_isBreakable = destructible;
 }
 
 
@@ -28,3 +34,18 @@ void Wall::SetBomb(bool hasBomb)
 {
 	m_hasBomb = hasBomb;
 }
+
+int Wall::getXStart() const { return m_xStart; }
+void Wall::setXStart(int xStart) { m_xStart = xStart; }
+
+int Wall::getYStart() const { return m_yStart; }
+void Wall::setYStart(int yStart) { m_yStart = yStart; }
+
+int Wall::getId() const { return m_id; }
+void Wall::setId(int id) { m_id = id; }
+
+bool Wall::isBreakable() const { return m_isBreakable; }
+void Wall::setBreakable(bool isBreakable) { m_isBreakable = isBreakable; }
+
+bool Wall::isVisible() const { return m_isVisible; }
+void Wall::setVisible(bool isVisible) { m_isVisible = isVisible; }
