4354ecc8edcfb1d131fff3df172957843ed0512e
Alecsa Alin <alin.alecsa@student.unitbv.ro>
2024-11-15 10:12:34 +0200

    Introduced QT

---
+++ Client/out/build/x64-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_TestamQT_FILE C:/Users/alinc/Documents/MC/out/build/x64-Debug/TestamQT.exe)
+set(__QT_DEPLOY_TARGET_TestamQT_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TestamQT_RUNTIME_DLLS C:/Qt/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
