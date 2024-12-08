abe01cb82fbea1c206813c5ad94d4be90ad148d3
andreirobert04 <barabasrobert3006@gmail.com>
2024-12-08 18:28:47 +0200

    GameWidget.h innit

--- QtWidgetsApplication/QtWidgetsApplication/GameWidget.cpp
+++ QtWidgetsApplication/QtWidgetsApplication/GameWidget.cpp
@@ -1,1 +1,22 @@
 #include "GameWidget.h"
+
+GameWidget::GameWidget(QWidget* parent)
+{
+}
+
+GameWidget::~GameWidget()
+{
+}
+
+void GameWidget::paintEvent(QPaintEvent* event)
+{
+}
+
+void GameWidget::keyPressEvent(QKeyEvent* event)
+{
+}
+
+void GameWidget::updateGame() 
+{
+
+}
--- QtWidgetsApplication/QtWidgetsApplication/GameWidget.h
+++ QtWidgetsApplication/QtWidgetsApplication/GameWidget.h
@@ -1,7 +1,24 @@
 #pragma once
 #include <QWidget>
+#include <QTimer>
 #include "Game.h"
-class GameWidget
+class GameWidget : public QWidget
 {
+	Q_OBJECT
+public:
+	explicit GameWidget(QWidget* parent = nullptr);
+	~GameWidget();
+
+protected:
+	void paintEvent(QPaintEvent* event) override;
+	void keyPressEvent(QKeyEvent* event) override;
+
+private:
+	Game m_game;
+	QTimer* m_timer; //timer pt actualizari
+	int m_titleSize = 20; //dim celulelor pe ecran
+
+private slots:
+	void updateGame();
 };
 
