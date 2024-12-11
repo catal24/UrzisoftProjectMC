c544a29d3bdbe18671881a27a5a7a21839107394
Borsan Iulian <iulian.borsan@student.unitbv.ro>
2024-12-11 18:09:20 +0200

    Leaderboard adjust, change wallpaper with game name in

--- ClientMC/ClientMC/LeaderboardDisplay.cpp
+++ ClientMC/ClientMC/LeaderboardDisplay.cpp
@@ -1,43 +1,61 @@
 #include "LeaderboardDisplay.h"
 
 LeaderboardDisplay::LeaderboardDisplay(QWidget* parent)
-	:QWidget(parent)
+    : QWidget(parent)
 {
-	this->setWindowTitle("Leaderboard");
-	this->setFixedSize(800, 600);
-	m_table = new QTableWidget(5,2,this);
-	m_table->setHorizontalHeaderLabels(QStringList() << "Name" << "Points");
-	m_table->horizontalHeader()->setStretchLastSection(true);
-	m_table->verticalHeader()->setVisible(true);
-	m_table->setShowGrid(true);
+    this->setWindowTitle("Leaderboard");
+    this->setFixedSize(800, 600);
 
-	m_table->setItem(0, 0, new QTableWidgetItem("1"));
-	m_table->setItem(0, 1, new QTableWidgetItem("2"));
-	m_table->setItem(1, 0, new QTableWidgetItem("3"));
-	m_table->setItem(1, 1, new QTableWidgetItem("4"));
-	m_table->setItem(2, 0, new QTableWidgetItem("5"));
-	m_table->setItem(2, 1, new QTableWidgetItem("6"));
-	m_table->setItem(3, 0, new QTableWidgetItem("7"));
-	m_table->setItem(3, 1, new QTableWidgetItem("8"));
-	m_table->setItem(4, 0, new QTableWidgetItem("9"));
-	m_table->setItem(4, 1, new QTableWidgetItem("10"));
+    m_table = new QTableWidget(5, 2, this);
+    m_table->setHorizontalHeaderLabels(QStringList() << "Name" << "Points");
+    m_table->horizontalHeader()->setStretchLastSection(true);
+    m_table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    m_table->verticalHeader()->setVisible(false);
+    m_table->setAlternatingRowColors(true);
+    m_table->setStyleSheet(
+        "QTableWidget { gridline-color: #D3D3D3; }"
+        "QHeaderView::section { background-color: #2E8B57; color: white; font-weight: bold; }"
+        "QTableWidget::item { selection-background-color: #ADD8E6; }"
+        "QTableWidget { border: 2px solid #8FBC8F; border-radius: 10px; }");
 
+    QFont font("Arial", 12, QFont::Bold);
+    m_table->setFont(font);
 
-	auto layout = new QVBoxLayout(this);
-	layout->addWidget(m_table);
-	layout->setContentsMargins(20, Qt::AlignVCenter, 20, 20);
-	QPushButton* backButton = new QPushButton("Back", this);
-	QVBoxLayout* layoutButton = new QVBoxLayout(this);
-	layoutButton->addWidget(backButton,0,Qt::AlignRight);
-	connect(backButton, &QPushButton::clicked, this, &LeaderboardDisplay::backButtonClicked);
-	connect(backButton, &QPushButton::clicked, this, [&]() {
-		this->hide();
+    m_table->setItem(0, 0, new QTableWidgetItem(""));
+    m_table->setItem(0, 1, new QTableWidgetItem(""));
+    m_table->setItem(1, 0, new QTableWidgetItem(""));
+    m_table->setItem(1, 1, new QTableWidgetItem(""));
+    m_table->setItem(1, 1, new QTableWidgetItem(""));
+    m_table->setItem(2, 0, new QTableWidgetItem(""));
+    m_table->setItem(2, 1, new QTableWidgetItem(""));
+    m_table->setItem(3, 0, new QTableWidgetItem(""));
+    m_table->setItem(3, 1, new QTableWidgetItem(""));
+    m_table->setItem(4, 0, new QTableWidgetItem(""));
+    m_table->setItem(4, 1, new QTableWidgetItem(""));
 
-		});
-	layout->addLayout(layoutButton);
+    for (int row = 0; row < m_table->rowCount(); ++row) {
+        for (int col = 0; col < m_table->columnCount(); ++col) {
+            QTableWidgetItem* item = m_table->item(row, col);
+            if (item) {
+                item->setFlags(item->flags() & ~Qt::ItemIsEditable);
+            }
+        }
+    }
 
-	setLayout(layout);
+    auto layout = new QVBoxLayout(this);
+    layout->addWidget(m_table);
+    layout->setContentsMargins(20, 20, 20, 20);
+
+    QPushButton* backButton = new QPushButton("Back", this);
+    backButton->setStyleSheet("QPushButton { background-color: #2E8B57; color: white; font-weight: bold; padding: 10px; }");
+    layout->addWidget(backButton, 0, Qt::AlignCenter);
+
+    connect(backButton, &QPushButton::clicked, this, &LeaderboardDisplay::backButtonClicked);
+    connect(backButton, &QPushButton::clicked, this, [&]() {
+        this->hide();
+        });
+
+    setLayout(layout);
 }
 
-LeaderboardDisplay::~LeaderboardDisplay()
-{}
+LeaderboardDisplay::~LeaderboardDisplay() {}
