f63d92d0b7ca84971945f79d256d8c049287a633
Borsan Iulian <iulian.borsan@student.unitbv.ro>
2025-01-13 23:03:53 +0200

    read from json good , and show 2 players in client

--- BattleCity/BattleCity/map.cpp
+++ BattleCity/BattleCity/map.cpp
@@ -6,14 +6,15 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+#include <crow/json.h>
+#include <vector>
 
 void setTextColor(int color) {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, color);
 }
 
-#include <crow/json.h>
-#include <vector>
+
 
 crow::json::wvalue Map::EncodeMap2() {
 	crow::json::wvalue encodedJson;
@@ -45,14 +46,14 @@ crow::json::wvalue Map::EncodeMap2() {
 	// Construim JSON-ul
 	for (size_t i = 0; i < encodedMap.size(); i++) {
 		for (size_t j = 0; j < encodedMap[i].size(); j++) {
-			encodedJson[""][i][j] = encodedMap[i][j];
+			encodedJson[i][j] = encodedMap[i][j];
 		}
 	}
 
 	return encodedJson;
 }
 
-std::ofstream outFile("encoded_map.txt");
+
 
 Map::Map(std::vector<std::vector<int>> mat)
 {
@@ -77,11 +78,8 @@ Map::Map(std::vector<std::vector<int>> mat)
 			}
 		}
 	}
-
-
-
-
 }
+
 
 void Map::DrawMap()
 {
@@ -105,6 +103,7 @@ void Map::DrawMap()
 
 std::vector<std::vector<int>> Map::EncodeMap()
 {
+	std::ofstream outFile("encoded_map.txt");
 	std::vector<std::vector<int>> encodedMap;
 	encodedMap.resize(m_map.size());
 
@@ -125,7 +124,6 @@ std::vector<std::vector<int>> Map::EncodeMap()
 			}
 		}
 	}
-
 	for (const auto& row : encodedMap) {
 		for (const auto& cell : row) {
 			outFile << cell << " ";
@@ -143,6 +141,7 @@ std::vector<std::vector<GameObject*>>& Map::GetMap()
 {
 	return m_map;
 }
+
 
 void Map::GenerateRandomBombsOnWalls(int numBombs)
 {
@@ -182,7 +181,6 @@ void Map::GenerateRandomBombsOnWalls(int numBombs)
 	}
 }
 
-
 void Map::GenerateRandomIndestructibleWalls(int count)
 {
 	int modified = 0;
@@ -214,7 +212,6 @@ void Map::SetIndestructibleBorders()
 			}
 }
 
-
 GameObject* Map::GetObjectAt(int x, int y)
 {
 	if (x >= 0 && x < m_height && y >= 0 && y < m_width) {
@@ -232,8 +229,6 @@ const std::vector<GameObject*>& Map::operator[](int index) const
 {
 	return m_map[index];
 }
-
-
 
 int Map::GetWidth() { return m_width; }
 int Map::GetHeight() { return m_height; }
